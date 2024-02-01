f38112d916a68d6160f57c68e6c84e2efa944cfe
unknown <ehtisham57>
2024-02-02 00:37:30 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Uipage from "./components/UiPage/Uipage";
+import MyComponent from "./components/sliderComponents/page";
+
+
+export default function Home() {
+  return (
+    <>
+      {/* <MyComponent /> */}
+
+      <Uipage />
+    </>
+  );
+}
